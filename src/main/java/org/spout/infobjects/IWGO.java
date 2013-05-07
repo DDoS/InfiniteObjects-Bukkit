335f1109a5c601575b9e1cf6babe3d9893a81857
Aleksi Sapon <alek42008@yahoo.com>
2013-05-07 18:16:01 -0400

    Bukkit Port: works!
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/infobjects/IWGO.java
+++ src/main/java/org/spout/infobjects/IWGO.java
@@ -33,7 +33,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import org.bukkit.Location;
 
 import org.bukkit.Location;
 import org.bukkit.World;
